5e8f2667a71d9d16529b9c6118b36285ff23765e
moldovexc <alexanderparpulansky@gmail.com>
2023-07-04 17:27:36 +0300

    Modified User Entity age property to be Transient

--- src/main/java/com/mm/libraryrestapi/entity/User.java
+++ src/main/java/com/mm/libraryrestapi/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Setter
@@ -27,11 +28,16 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
-
+    @Transient
     private long age;
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
     private String gender;
+    @Column(nullable = false)
     private String address;
+    @Column(nullable = false)
     private String city;
+    @Column(nullable = false)
     private String country;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
