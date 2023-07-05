e3a83935d8108efc3e7ebd4093baaa9b92a14da8
ruben-izembrandt <ruben.izembrandt@university.mentormate.com>
2023-07-05 15:38:37 -0400

    Added function to postpone the return date of a book

--- src/main/java/com/mm/libraryrestapi/controller/BorrowBookController.java
+++ src/main/java/com/mm/libraryrestapi/controller/BorrowBookController.java
@@ -2,13 +2,12 @@ package com.mm.libraryrestapi.controller;
 
 import com.mm.libraryrestapi.payload.BorrowHistoryDto;
 import com.mm.libraryrestapi.services.BorrowBookService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/borrow")
+@RequestMapping("/api/v1/")
 public class BorrowBookController {
 
     private final BorrowBookService borrowBookService;
@@ -17,8 +16,14 @@ public class BorrowBookController {
         this.borrowBookService = borrowBookService;
     }
 
-    @PostMapping("{bookId}")
-    public ResponseEntity<BorrowHistoryDto> borrowPaperBook(@PathVariable Long bookId, @Valid @RequestBody BorrowHistoryDto borrowHistoryDto) {
-        return new ResponseEntity<>(borrowBookService.borrowBook(bookId, borrowHistoryDto), HttpStatus.CREATED);
+    @PostMapping("borrow/{bookId}")
+    public ResponseEntity<BorrowHistoryDto> borrowPaperBook(@PathVariable Long bookId) {
+        return new ResponseEntity<>(borrowBookService.borrowBook(bookId), HttpStatus.CREATED);
     }
+    @PatchMapping("/{borrowId}/postpone")
+    public ResponseEntity<BorrowHistoryDto> borrowPaperBook(@PathVariable Long borrowId, @RequestParam("days") Long days) {
+        return new ResponseEntity<>(borrowBookService.postponeReturnDate(borrowId, days), HttpStatus.CREATED);
+    }
+
+
 }
