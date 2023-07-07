9b86eaab32de356f1e92e40bc7acb95de05e0f38
Daniel Baykov <danibaikov@gmail.com>
2023-07-07 16:36:16 +0300

    fixing name tag

--- src/main/java/com/mm/libraryrestapi/controller/UserCloudHistoryController.java
+++ src/main/java/com/mm/libraryrestapi/controller/UserCloudHistoryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@Tag(name = "CRUD REST APIs for User Clod History Resource")
+@Tag(name = "CRUD REST APIs for User Cloud History Resource")
 public class UserCloudHistoryController {
     private final UserCloudHistoryService userCloudHistoryService;
 
